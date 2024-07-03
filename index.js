af41514d3b122671ef42b056bf36d4baa30693fa
Yogesh07-pixel <yogeshrawat518@gmail.com>
2024-07-03 14:47:25 +0530

    Updated

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const { connectmongoDB } = require("./connection");
+
 const { logReqRes } = require("./middlewares");
+
 const userRouter = require("./routes/user");
 
 const app = express();
 const port = 8080;
 
 // Connection to MongoDB
-connectmongoDB("mongodb://127.0.0.1:27017/Backend-app-1")
-  .then(() => console.log("MongoDB Connected Successfully"))
-  .catch((err) => console.log("Error", err));
+connectmongoDB("mongodb://127.0.0.1:27017/Backend-app-1").then(() => {
+  console.log("MongoDB Connected!");
+});
 
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
 
 // Routes
-app.use("/user", userRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server Started at ${port} Successfully`));
